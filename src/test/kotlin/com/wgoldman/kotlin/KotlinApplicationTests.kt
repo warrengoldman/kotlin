5f4b528758967db41d911b82a0cc29bff0a87582
Warren Goldman <Warren.Goldman@pacelabs.com>
2019-11-08 07:56:14 -0600

    initial app

---
+++ src/test/kotlin/com/wgoldman/kotlin/KotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.wgoldman.kotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
