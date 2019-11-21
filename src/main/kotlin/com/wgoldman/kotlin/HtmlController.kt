57d3865ac8b59ea154c761fca8a59dd47b4c59be
Warren Goldman <Warren.Goldman@pacelabs.com>
2019-11-21 15:49:46 -0600

    attempted post, but cannot do directly from html

--- src/main/kotlin/com/wgoldman/kotlin/HtmlController.kt
+++ src/main/kotlin/com/wgoldman/kotlin/HtmlController.kt
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 
 @Controller
 class HtmlController {
@@ -13,5 +15,10 @@ class HtmlController {
     model["title"] = "Blog"
     return "blog"
   }
-
+  @PostMapping("/saveComment")
+  fun saveComment(model: Model, @RequestBody comment: CommentForm): String {
+	  model["title"] = "live" + comment.comment
+	  return "blog"
+  }
+  data class CommentForm (val comment: String)
 }
