f56e22083ef5eae12ff792d150e911e3e562bc4b
warrengoldman <Warren.Goldman@pacelabs.com>
2020-01-15 10:32:31 -0600

    got submit working

--- src/main/kotlin/com/wgoldman/kotlin/HtmlController.kt
+++ src/main/kotlin/com/wgoldman/kotlin/HtmlController.kt
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.MediaType;
 
 @Controller
 class HtmlController {
@@ -28,11 +29,10 @@ class HtmlController {
     return "configServerReport"
   }
 
-  @PostMapping("/saveComment")
-  fun saveComment(model: Model, @RequestBody comment: CommentForm): String {
-	  model["title"] = "live" + comment.comment
+  @PostMapping("/saveComment", consumes=arrayOf("text/plain"))
+  fun saveComment(model: Model, @RequestBody commentForm: String): String {
+	  model["title"] = "live" + commentForm
 	  return "blog"
   }
-  data class CommentForm (val comment: String)
   data class AppData (val appContext: String, val appEnv: String, val httpProp: String)
 }
